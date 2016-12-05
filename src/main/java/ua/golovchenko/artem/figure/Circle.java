f6d92e175667e7295b0c5719fd2831b2ed042cdb
Artem Golovchenko <golovchenko84@mail.ru>
2016-12-05 20:16:07 +0200

    добавил класс Circle

---
+++ src/main/java/ua/golovchenko/artem/figure/Circle.java
@@ -0,0 +1,7 @@
+package ua.golovchenko.artem.figure;
+
+/**
+ * Created by головченко on 05.12.2016.
+ */
+public class Circle {
+}
---
+++ src/test/java/ua/golovchenko/artem/figure/CircleTest.java
@@ -0,0 +1,18 @@
+package ua.golovchenko.artem.figure;
+
+
+import org.junit.Before;
+
+/**
+ * Created by головченко on 05.12.2016.
+ */
+public class CircleTest {
+    Circle circle;
+
+    @Before
+    public void setUp(){
+        circle = new Circle();
+    }
+
+
+}
