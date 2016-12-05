3b2c8c401054778c6be1d4d3e4ce2457c71bd264
Artem Golovchenko <golovchenko84@mail.ru>
2016-12-05 20:07:26 +0200

    Подготовил класс Figure & Coordinats для тестирования Mock

---
+++ src/main/java/ua/golovchenko/artem/figure/Coordinats.java
@@ -0,0 +1,23 @@
+package ua.golovchenko.artem.figure;
+
+/**
+ * Created by головченко on 05.12.2016.
+ */
+public class Coordinats {
+    private int x;
+    private int y;
+
+    public Coordinats(int x, int y) {
+
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
